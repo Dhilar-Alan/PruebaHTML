582be35c60e6f3f8760c12b62490cda27bb97876
Alan Garcia <32755506+Dhilar-Alan@users.noreply.github.com>
2021-08-12 17:43:29 -0600

    Create laboratorio.php

---
+++ laboratorio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>ServiCal</title>
+	<link rel="stylesheet" href="stilos.css">
+</head>
+<body>
+	<div class="contenedor">
+			<legend> <h1>Patrones - Instalaciones - Servicio</h1></legend>
+
+		<nav id="navegacion">
+            		<ul>
+            			<li><a href="index.php">Inicio</a></li>
+            			<li><a href="Servicios.php">Servicios</a></li>
+            			<li><a href="laboratorio.php">Laboratorio</a></li>
+            		</ul>
+    	</nav>	
+	</div>
+	<div class="body">
+		<h3>Aqui laboratorio</h3>
+	</div>
+
+</body>
+</html>
