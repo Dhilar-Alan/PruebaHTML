6f22f377b336fee5e0118ce19637877ff0b67f33
Alan Garcia <32755506+Dhilar-Alan@users.noreply.github.com>
2021-08-12 17:43:01 -0600

    Create Servicios.php

---
+++ Servicios.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>ServiCal</title>
+	<link rel="stylesheet" href="hola2.css">
+</head>
+<body>
+	<div class="contenedor">
+			<legend> <h1>Diversos Servicios que se ofrecen</h1></legend>
+
+		<nav id="navegacion">
+            		<ul>
+            			<li><a href="index.php">Inicio</a></li>
+            			<li><a href="Servicios.php">Servicios</a></li>
+            			<li><a href="laboratorio.php">Laboratorio</a></li>
+            		</ul>
+    	</nav>	
+	</div>
+	<div class="fili">
+
+<div id="box">
+<div class="colp6 coli6">
+<div class="cosita">
+       <div  id = "highlight">Presion </p></div>
+        <p id="p1"> 
+         Calibracion de Manometros - Manovacuometros - Vacuometros - Switch de presion - Manometros Diferenciales - Presostatos - Valvulas de seguridad - Transmisores de presion.
+         </p>
+       </div>
+     </div>
+<div class="colp6 coli6">    
+<div class="cosita">         
+		<div id = "highlight2">Iluminacion y Velocidad</p></div>
+		 <p id="p2">
+          Ofrecemos el servicio de lecturas para saber con certeza la cantidad de lux que tiene en algun area en especifico, podra garantizar usted que se encuentra dentro de los parametros necesarios para continuar sus operaciones.
+
+          Velocidad, se obrece el servicio de lecturas de velocidad, a cualquier instrumento equipo que necesite saber con que velocidad marcha y realiza las operaciones.
+         </p>
+       </div>
+     </div>
+<div class="colp6 coli6">
+<div class="cosita">
+<div id = "highlight3">Calificacion</p></div>
+<p id="p3">
+          Serie de pruebas para garantizar el buen funcionamiento de sus instrumentos, inclusive pruebas fisico-quimicas a los mismos, probando cada etapa del funcionamiento de los mismos.
+        </p>
+        </div>
+     </div>
+<div class="colp6 coli6">   
+<div class="cosita">        
+<div id = "highlight4">Mantenimiento</p></div>
+		 <p id="p4">
+          Mantenimiento a todo instrumento y equipo a nivel lavoratorio e industria, enfocados en brindarles un buen Healtchec a los equpos, garantizando una extensa vida util, retrasando tiempos de paros de uso.
+         </p>
+       </div>
+     </div>
+     <div class="body">
+     <img src="img/logo.jpg">
+	</div>
+</body>
+</html>
