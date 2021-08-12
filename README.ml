c9a66122d81dac02be5b35634e9a78f8950f08ab
Alan Garcia <32755506+Dhilar-Alan@users.noreply.github.com>
2021-08-12 17:49:36 -0600

    Update README.ml

--- README.ml
+++ README.ml
@@ -1,6 +1,4 @@
 # PruebaHTML
 Pagina Estatica realizada para testear el funcionamiento de GitHub pages
 Your site is published at https://dhilar-alan.github.io/PruebaHTML/
-<div class="body">
-		<img src="img/icono_servical.jpg">
-	</div>
+
